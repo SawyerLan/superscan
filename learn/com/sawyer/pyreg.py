ccfe2f46be3eef1d97f215588d7c0faa529bf650
SawyerLan <1027950400@qq.com>
2015-12-17 14:13:08 +0800

    第一次提交

---
+++ learn/com/sawyer/pyreg.py
@@ -0,0 +1,17 @@
+'''
+Created on 2015-12-17
+
+@author: SawyerLan
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
